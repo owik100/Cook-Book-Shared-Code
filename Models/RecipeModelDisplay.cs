9a17fefa48537585ea7fbb9c3840cee1016335c3
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2020-04-21 11:19:59 +0200

    Display your own recipes on public recipes

---
+++ Models/RecipeModelDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cook_Book_Shared_Code.Models
+{
+    public class RecipeModelDisplay
+    {
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string> Ingredients { get; set; }
+        public string Instruction { get; set; }
+        public string NameOfImage { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsPublic { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public bool DisplayAsPublic { get; set; }
+    }
+}
